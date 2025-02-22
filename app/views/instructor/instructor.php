958503a3594dfd912010dd413f4df2da05238799
saneeshast <saneeshatharindist@gmail.com>
2025-02-22 21:30:01 +0530

    instructor dashboard edited

--- app/views/instructor/instructor.php
+++ app/views/instructor/instructor.php
@@ -31,111 +31,140 @@ else{
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Company Dashboard</title>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script src="https://kit.fontawesome.com/1c1508aefb.js" crossorigin="anonymous"></script>
+    <title>| Instructor |</title>    
+    <!-- <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/user/main.css">
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/user/custom.css">
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/user/buttons.css">
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/user/edit.css"> 
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/user/custom.css">
+    
+    -->
+    
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/user/dashboard.css"> 
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/user/sidebar.css">
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/user/modals.css"> 
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/user/tables.css"> 
+
+
+
+    <script src="<?= ROOT ?>/assets/js/user/user_1.js" defer></script>
+    <script src="<?= ROOT ?>/assets/js/user/user_2.js" defer></script>
+
+</head> 
+<body>
+<div class="main-container">
+
+
+<!-- Sidebar Menu -->
+<div class="sidebar">
+    <br>
+
+        <header>
+            <img src="<?= ROOT ?>/assets/images/instructor/profile/images/<?php echo $profile_image; ?>" alt="logo" class="logo">
+        </header>
+
+            <div class="nav-links">
+                <ul>
+                    <div class="grps">
+                        <li class="tabs activetab" value="1"><a><i class="fa fa-home"></i>Home</a></li>
+                        <li class="tabs " value="2"><a><i class="bi bi-person-circle"></i>Profile</a></li>
+                        <li class="tabs" value="3"><a><i class="bi bi-stars"></i>Gym</a></li>
+                       
+                        <li class="tabs" value="4"><a><i class="bi-person-arms-up"></i>Member Profiles</a></li>
+                        <li class="tabs" value="5"><a><i class="bi bi-calendar2-check-fill"></i>Appoinments</a></li>
+                        
+                        <li class="tabs" value="6"><a><i class="bi bi-credit-card-fill"></i>Payments</a></li>
+                        <li class="tabs" value="7"><a><i class="bi bi-chat-heart-fill"></i>Support</a></li>
+
+
+                    </div>
+
+                   
+                    
+
+                    
+                    
+                    <!-- <li class="tabs" value="10"><a><i class="bi bi-gear-fill"></i>Search Gyms</a></li> -->
+                    <!-- <li class="tabs" value="4"><a><i class="bi bi-stack-overflow"></i>Materials</a></li> -->
+                    <!-- <li class="tabs" value="11"><a><i class="bi bi-chat-left-heart-fill"></i>Posts</a></li>-->
+                    <!-- <li class="tabs" value="6"><a><i class="bi bi-book-fill"></i>Reports</a></li> -->
+                    <!-- <li class="tabs" value="7"><a><i class="bi bi-alarm-fill"></i>Reminders</a></li> -->
+
+                    
+                   
+                    <div class="grps">
+                        
+                        <li class="tabs" value=""> 
+                                <a href="<?= ROOT ?>/login/logout" class="logout-btn">
+                                <i class="fa-solid fa-right-from-bracket"></i>
+                                Logout
+                            </a>
+                        </li>
+                    </div>
+
+                </ul>
+            </div> <!-- End of navlink-->
+</div> <!-- End of Sidebar Menu -->
+
+
+<!-- Main Content -->
+  
+<div class="descriptor active" value="1">
+    <?php
+                require 'home.php';
+    ?>
+    </div> 
+
+    <div class="descriptor " value="2">
+    <?php
+                require 'profile.php';
+    ?>  
+    </div> 
+
+
+   
+    <div class="descriptor" value="3">
+    <?php
+                require 'Gym.php';
+               
+
+    ?>
+    </div>
+
+
+   
+    <div class="descriptor" value="4">
+       <?php
+                require 'members.php';
+       ?>
+    </div>
+
+    <div class="descriptor" value="5">
+    <?php
+                require 'schedule.php';
+    ?>
+    </div>
+
+    <div class="descriptor" value="6">
+    <?php
+                require 'payment.php';
+    ?>
+    </div> 
+
+    <div class="descriptor" value="7">
+    <?php
+                require 'support.php';
+    ?>
+    </div>
 
     
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-
-    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/instructor/main.css">
-    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/instructor/custom.css">
-    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/instructor/edit.css">
-    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/assets/css/instructor/css/buttons.css">
-    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    <script src="https://kit.fontawesome.com/1c1508aefb.js" crossorigin="anonymous"></script>
-    <script src="<?= ROOT ?>/assets/js/instructor/instructor_1.js" defer></script>
-    <script src="<?= ROOT ?>/assets/js/instructor/instructor_2.js" defer></script>
-</head>
-<body>
-    <!-- Header Section -->
-    <header class="header">
-
-        <i class="bi bi-house btn btn-primary"></i>
-
-        <nav class="navbar">
-            <a href="<?= ROOT ?>/login/logout" class="logout-btn">
-                <i class="fa-solid fa-right-from-bracket"></i>Logout
-            </a>
-        </nav>
-    </header> <!-- End of Header -->
-
-    <!-- Container -->
-    <div class="container">
-        <!-- Sidebar Menu -->
-        <div class="m-lg-0" >
-        <div class="sidemenu">
-            <br>
-            <div class="container-fluid">
-            <img src="<?= ROOT ?>/assets/images/instructor/profile/images/<?php echo $profile_images; ?>" alt="logo" class="logo">
-            <div class="menu">
-                <ul>
-                    <li class="tabs activetab" value="1"><a><i class="bi bi-person-circle"></i>Profile</a></li>
-                    <li class="tabs" value="2"><a><i class="bi bi-calendar2-check-fill"></i>Schedule</a></li>
-                    <li class="tabs" value="3"><a><i class="bi bi-person-lines-fill"></i>Contacts</a></li>
-                    <li class="tabs" value="4"><a><i class="bi bi-alarm-fill"></i>Reminder</a></li>
-                    <li class="tabs" value="5"><a><i class="bi bi-stack-overflow"></i>Materials</a></li>
-                    <li class="tabs" value="6"><a><i class="bi bi-chat-heart-fill"></i>Support</a></li>
-        
-                </ul>
-            </div> <!-- End of Menu -->
-            </div>
-        </div> <!-- End of Sidebar Menu -->
-      </div>
-
-        <!-- Main Content --> 
-        <div class="content">
-                        
-        
-            <h1>Welcome, <?php echo $trainer_username; ?>!</h1>
-
-
-            <!-- Profile Section -->
-            <div class="descriptor active" value="1">
-            <?php
-                        require 'profile.php';
-            ?>  
-            </div> 
-
-
-            <div class="descriptor" value="2">
-                <h2>Schedule</h2>
-                <hr>
-                <div class="jobreq"></div>
-            </div> 
-
-
-            <div class="descriptor" value="3">
-            <?php
-                        require 'contacts.php';
-            ?>  
-            </div> 
-
-
-            <div class="descriptor" value="4">
-            <?php
-                        require 'reminders.php';
-            ?>  
-            </div> 
-        
-        
-            <div class="descriptor" value="5">
-            <?php
-                            require 'materials.php';
-                ?>  
-            </div> 
-
-
-             <div class="descriptor" value="6">
-             <?php
-                        require 'support.php';
-            ?>  
-            </div> 
-
-
-
-        </div> <!-- End of Main Content -->
-    </div> <!-- End of Container -->
+   
+</div>       
 </body>
 </html>
