e551b66fd2232740c0a715a131466c1f2227512a
saneeshast <saneeshatharindist@gmail.com>
2025-02-23 22:03:59 +0530

    instructor dashboard updated

---
+++ app/views/instructor/payment.php
@@ -0,0 +1,20 @@
+<div class="in-content">
+
+        <div class="header">
+        <div>
+
+        <h2>My Income</h2>
+        <p>
+            <?php
+            echo date("l, F j, Y"); // Outputs: Wednesday, December 18, 2024
+            ?></p>
+
+        </div>
+        </div>
+
+<div class="in-in-content">
+
+<h3> payment to be implemented </h3>
+
+</div>
+</div>
---
+++ app/views/instructor/schedule.php
@@ -0,0 +1,20 @@
+<div class="in-content">
+
+        <div class="header">
+        <div>
+
+        <h2>Appinments</h2>
+        <p>
+            <?php
+            echo date("l, F j, Y"); // Outputs: Wednesday, December 18, 2024
+            ?></p>
+
+        </div>
+        </div>
+
+<div class="in-in-content">
+
+<h3> appoinments to be implemented </h3>
+
+</div>
+</div>
