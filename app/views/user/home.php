b8a5d655daadba2750c348175100baf899a57108
BSathurshan <2022is089@stu.ucsc.cmb.ac.lk>
2025-02-23 18:49:15 +0530

    user's ui is improved(re added navigations $ improved css)

--- app/views/user/home.php
+++ app/views/user/home.php
@@ -1,35 +1,17 @@
 <div class="in-content">
 
-<div class="header">
-        <div>
+        <div class="header">
+                <div>
 
-        <h2>Home</h2>
-
-
-        </div>
-        </div>
-<div class="in-in-content">
-        <button class='editBtn' onclick="createTicket('<?php echo $username; ?>')">Create Ticket</button>
-        <!-- Hidden Edit Form (Modal) -->
-        <div id="SupportFormModal" class="modal" style="display: none;">
-                        <div class="modal-content">
-                        <h3>Support</h3>
-                        <form id="editForm" method="POST" action="<?= ROOT ?>/user/getSupport" enctype="multipart/form-data">
-                            <input type="hidden" name="username" id="USER_NAME">
-                            <input type="hidden" name="access" value="instructor">
-                            <input type="hidden" id="email" name="email" value='<?php echo $email; ?>' required>
-
-
-                            <label for="title">Issue:</label>
-                            <input type="text" name="issue" id="issue" required><br>
-
-                            <label for="details">Details:</label>
-                            <textarea name="details" id="details" rows="4" cols="50" required></textarea><br>
-
-                            <input type="submit" value="Send">
-                            <button type="button" onclick="closeEditModal()">Cancel</button>
-                </form>
+                <h2>Home</h2>
                 </div>
         </div>
+        
+        <div class="in-in-content">
+
+                <h3> Home to be implemented </h3>
+
+                
+        </div>
     </div>
-</div>        
+      
